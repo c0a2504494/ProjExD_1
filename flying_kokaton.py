614ce96b78a70f3b69e20bfcc5241adfb71a8f48
oosawa <c0a2504494@edu.teu.ac.jp>
2026-06-09 16:27:24 +0900

    ensyuu_3

---
+++ flying_kokaton.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_flip = pg.transform.flip(bg_img, True, False)
+
+
+    gazoukadai = pg.image.load("fig/3.png")
+    gazoukadai = pg.transform.flip(gazoukadai,True,False)
+    
+
+    tmr = 0
+    i=0
+    k=0
+    kk_rct=gazoukadai.get_rect()
+    kk_rct.center = 300, 200
+    
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        if k % 2 == 0:
+            screen.blit(bg_img, [i, 0])
+            screen.blit(bg_img_flip, [i+1600, 0])
+        else:
+            screen.blit(bg_img_flip, [i, 0])
+            screen.blit(bg_img, [i+1600, 0])
+
+        key_list=pg.key.get_pressed()
+        x=0
+        y=0
+        if key_list[pg.K_UP]:
+            y=-1
+        if key_list[pg.K_DOWN]:
+            y=1
+        if key_list[pg.K_LEFT]:
+            x=-1
+        if key_list[pg.K_RIGHT]:
+            x=1
+        else:
+            x=-1 
+        kk_rct.move_ip(x,y)
+        screen.blit(gazoukadai, [kk_rct.x, kk_rct.y])
+        i-=1
+        
+
+
+
+        
+        
+        if i==-1600:
+            i=0
+            k+=1
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+    
+
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
