192603649bd29e72275b8d6bf73cbea9778eeeaf
oosawa <c0a2504494@edu.teu.ac.jp>
2026-06-09 15:35:26 +0900

    練習3と4：こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,20 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+
+
+    gazoukadai = pg.image.load("fig/3.png")
+    gazoukadai = pg.transform.flip(gazoukadai,True,False)
+    
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(gazoukadai, [300, 200])
+
         pg.display.update()
         tmr += 1        
         clock.tick(10)
